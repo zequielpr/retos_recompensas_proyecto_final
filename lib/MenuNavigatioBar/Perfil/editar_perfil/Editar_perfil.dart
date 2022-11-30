133d69afc432acdf448f75a76c08bff87452fd09
zequielpr <zequielperez.r@gmail.com>
2022-11-30 13:51:04 +0000

    El usuario puede cambiar su nombre

--- lib/MenuNavigatioBar/Perfil/editar_perfil/Editar_perfil.dart
+++ lib/MenuNavigatioBar/Perfil/editar_perfil/Editar_perfil.dart
@@ -52,7 +52,7 @@ class _EditarPerfilState extends State<EditarPerfil> {
                 leading: Icon(Icons.edit),
                 onTap: () => context.router.push(ModificarNombreRouter()),
                 visualDensity: VisualDensity.compact,
-                title: DatosPersonales.getDato(CurrentUser.getIdCurrentUser(), 'nombre'),
+                title:Text(CurrentUser.currentUser?.displayName as String),
                 trailing: Icon(
                   Icons.arrow_forward_ios_sharp,
                   size: arrowSize,
