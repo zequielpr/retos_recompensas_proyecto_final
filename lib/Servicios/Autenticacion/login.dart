9c3bfc48c18966ec4e7e3a7b97003e2e01ab20d8
zequielpr <zequielperez.r@gmail.com>
2022-12-23 00:21:20 +0000

    Ajustes de diseño realizados en el diseño del loging

--- lib/Servicios/Autenticacion/login.dart
+++ lib/Servicios/Autenticacion/login.dart
@@ -14,6 +14,7 @@ import 'package:retos_proyecto/Rutas.gr.dart';
 import 'package:retos_proyecto/Servicios/Autenticacion/EmailPassw/IniciarSessionEmailPassw.dart';
 
 import '../../Colores.dart';
+import '../../Loanding.dart';
 import '../../MediaQuery.dart';
 import '../../datos/TransferirDatos.dart';
 import '../Notificaciones/AdministrarTokens.dart';
@@ -99,32 +100,7 @@ class _LoginState extends State<Login> {
                     }
      */
 
-    var bodyProgress = Container(
-      child: Stack(
-        children: <Widget>[
-          body,
-          Container(
-            alignment: AlignmentDirectional.center,
-            decoration: const BoxDecoration(),
-            child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.blue[200],
-                  borderRadius: BorderRadius.circular(10.0)),
-              width: 50.0,
-              height: 50.0,
-              alignment: AlignmentDirectional.center,
-              child: Transform.scale(
-                scale: 0.7,
-                child: const CircularProgressIndicator(
-                  color: Colors.grey,
-                  strokeWidth: 5,
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
+    var bodyProgress = Loanding.getLoanding(body, context);
 
     return Scaffold(
       body: _loading ? bodyProgress : body,
@@ -283,8 +259,7 @@ class _LoginState extends State<Login> {
             const TextSpan(text: 'y confirmas haber leido la '),
             TextSpan(
                 text: 'politica de privacidad.',
-                style:
-                    const TextStyle(color: Colores.colorPrincipal),
+                style: const TextStyle(color: Colores.colorPrincipal),
                 recognizer: TapGestureRecognizer()
                   ..onTap = _politicaDePrivacidad),
           ]),
@@ -294,32 +269,31 @@ class _LoginState extends State<Login> {
   Widget _getFooter() {
     return Container(
       height: Pantalla.getPorcentPanntalla(8, context, 'y'),
-      color: const Color.fromARGB(202, 217, 217, 217),
+      //color: const Color.fromARGB(202, 217, 217, 217),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Divider(indent: 1, endIndent: 1, color: Colors.black),
-
           ElevatedButton(
-              style: ButtonStyle(
-                  elevation: MaterialStateProperty.all(0),
-                  backgroundColor:
-                      MaterialStateProperty.all(Colors.transparent)),
-              onPressed: () {
-                TranferirDatosRoll datos =
-                    TranferirDatosRoll('userContraseña', collecUsuarios);
-                _irRollPage(datos);
-              },
-              child: RichText(
-                text: const TextSpan(children: [
-                  TextSpan(
-                      text: '¿No tienes una cuenta?',
-                      style: TextStyle(color: Colors.black)),
-                  TextSpan(
-                      text: '   Registrarse',
-                      style: TextStyle(color: Colores.colorPrincipal))
-                ]),
-              ))
+            style: ButtonStyle(
+                elevation: MaterialStateProperty.all(0),
+                backgroundColor: MaterialStateProperty.all(Colors.transparent)),
+            onPressed: () {
+              TranferirDatosRoll datos =
+                  TranferirDatosRoll('userContraseña', collecUsuarios);
+              _irRollPage(datos);
+            },
+            child: RichText(
+              text: const TextSpan(children: [
+                TextSpan(
+                    text: '¿No tienes una cuenta?',
+                    style: TextStyle(color: Colors.black)),
+                TextSpan(
+                    text: '   Registrarse',
+                    style: TextStyle(color: Colores.colorPrincipal))
+              ]),
+            ),
+          ),
+          Divider(),
         ],
       ),
     );
