9e4a0e10c2b7732e95d1f3dd4d53c1237b536155
zequielpr <zequielperez.r@gmail.com>
2023-01-30 02:48:53 +0000

    Salir de la sala al abandonar la misma

--- lib/MenuNavigatioBar/Salas/Tutorado/ListMisiones.dart
+++ lib/MenuNavigatioBar/Salas/Tutorado/ListMisiones.dart
@@ -1,3 +1,4 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:retos_proyecto/MediaQuery.dart';
